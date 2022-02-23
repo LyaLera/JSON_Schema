37b752d97976a30e50f3b8e63b1ce92c9f98c03b
Lera Lyakisheva <leralyakisheva@gmail.com>
2022-02-23 22:06:05 +0300

    Structure changes

--- src/test/java/ru/netology/PostRequestTest.java
+++ src/test/java/ru/netology/PostRequestTest.java
@@ -1,9 +1,11 @@
+package ru.netology;
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-public class PostRequest {
+public class PostRequestTest {
     @Test
     void shouldReturnPostedBody() {
 
